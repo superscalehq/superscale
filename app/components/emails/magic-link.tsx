a0096408eea8585973ea70e32d0be999747e3bcb
iantanwx <ian.tan@voyage.finance>
2023-09-16 13:48:46 +0800

    feat: polish up magic link email

--- app/components/emails/magic-link.tsx
+++ app/components/emails/magic-link.tsx
@@ -1,137 +1,77 @@
 import {
   Body,
   Container,
+  Section,
   Head,
   Heading,
   Html,
   Img,
   Link,
   Preview,
+  Tailwind,
   Text,
+  Button,
+  Hr,
 } from '@react-email/components';
 import * as React from 'react';
 
 interface MagicLinkEmailProps {
-  loginCode?: string;
+  link?: string;
 }
 
 const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : '';
 
-export const MagicLinkEmail = ({ loginCode }: MagicLinkEmailProps) => (
+export const MagicLinkEmail = ({
+  link = 'https://superscale.app',
+}: MagicLinkEmailProps) => (
   <Html>
     <Head />
-    <Preview>Log in with this magic link</Preview>
-    <Body style={main}>
-      <Container style={container}>
-        <Heading style={h1}>Login</Heading>
-        <Link
-          href="https://superscale.app"
-          target="_blank"
-          style={{
-            ...link,
-            display: 'block',
-            marginBottom: '16px',
-          }}
-        >
-          Click here to log in with this magic link
-        </Link>
-        <Text style={{ ...text, marginBottom: '14px' }}>
-          Or, copy and paste this temporary login code:
-        </Text>
-        <code style={code}>{loginCode}</code>
-        <Text
-          style={{
-            ...text,
-            color: '#ababab',
-            marginTop: '14px',
-            marginBottom: '16px',
-          }}
-        >
-          If you didn&apos;t try to login, you can safely ignore this email.
-        </Text>
-        <Img
-          src={`${baseUrl}/static/logo.png`}
-          width="32"
-          height="32"
-          alt="Notion's Logo"
-        />
-        <Text style={footer}>
-          <Link
-            href="https://superscale.app"
-            target="_blank"
-            style={{ ...link, color: '#898989' }}
-          >
-            Notion.so
+    <Preview>Sign in to Superscale with this magic link</Preview>
+    <Tailwind>
+      <Body className="mx-auto my-auto bg-white font-sans">
+        <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+          <Section className="mt-[32px]">
+            <Img
+              src={`${baseUrl}/static/logo.png`}
+              width={80}
+              height={80}
+              className="mx-auto"
+            />
+          </Section>
+          <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+            Login to <strong>Superscale</strong>
+          </Heading>
+          <Text>
+            Click the link below to log in. It will only be valid for a short
+            time.
+          </Text>
+          <Section className="mb-[32px] mt-[32px] text-center">
+            <Button
+              pX={20}
+              pY={12}
+              className="rounded bg-[#000000] text-center text-[12px] font-semibold text-white no-underline"
+              href={link}
+            >
+              Login
+            </Button>
+          </Section>
+          <Text>
+            Alternatively, you can visit the URL in your browser directly:
+          </Text>
+          <Link href={link} className="text-blue-600 no-underline">
+            {link}
           </Link>
-          , the all-in-one-workspace
-          <br />
-          for your notes, tasks, wikis, and databases.
-        </Text>
-      </Container>
-    </Body>
+          <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+          <Text className="text-[12px] leading-[24px] text-[#666666]">
+            This email was sent because a login with your email was requested.
+            If you didn&apos;t try to login, you can safely ignore this email.
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
   </Html>
 );
 
-MagicLinkEmail.PreviewProps = {
-  loginCode: 'sparo-ndigo-amurt-secan',
-} as MagicLinkEmailProps;
-
 export default MagicLinkEmail;
-
-const main = {
-  backgroundColor: '#ffffff',
-};
-
-const container = {
-  paddingLeft: '12px',
-  paddingRight: '12px',
-  margin: '0 auto',
-};
-
-const h1 = {
-  color: '#333',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '24px',
-  fontWeight: 'bold',
-  margin: '40px 0',
-  padding: '0',
-};
-
-const link = {
-  color: '#2754C5',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '14px',
-  textDecoration: 'underline',
-};
-
-const text = {
-  color: '#333',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '14px',
-  margin: '24px 0',
-};
-
-const footer = {
-  color: '#898989',
-  fontFamily:
-    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-  fontSize: '12px',
-  lineHeight: '22px',
-  marginTop: '12px',
-  marginBottom: '24px',
-};
-
-const code = {
-  display: 'inline-block',
-  padding: '16px 4.5%',
-  width: '90.5%',
-  backgroundColor: '#f4f4f4',
-  borderRadius: '5px',
-  border: '1px solid #eee',
-  color: '#333',
-};
