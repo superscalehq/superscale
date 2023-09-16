d44c232d5b31db8fa2c7144f79fdaf5bc84e79fe
iantanwx <ian.tan@voyage.finance>
2023-09-16 13:14:08 +0800

    feat: polish up check email page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/global.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import cn from 'classnames';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          inter.className,
+          'min-h-screen bg-background font-sans antialiased'
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/sign-in/check-email/page.tsx
+++ app/sign-in/check-email/page.tsx
@@ -1,5 +1,5 @@
-import { useSearchParams } from 'next/navigation';
 import * as z from 'zod';
+import Image from 'next/image';
 
 const schema = z.object({
   email: z.string().email(),
@@ -11,29 +11,39 @@ interface Props {
 
 export default function CheckEmailPage({ searchParams }: Props) {
   const params = schema.safeParse(searchParams);
-  console.log(searchParams);
-  // @ts-ignore
-  console.log(params.error);
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      {params.success ? (
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-semibold leading-3">Check your email</h1>
-          <p className="text-center text-muted-foreground">
-            We've sent an email to {params.data.email}. Click the link in the
-            email to confirm your account.
-          </p>
+    <div className="container h-full">
+      <div className="flex h-full w-full flex-row items-center justify-center">
+        <div className="flex flex-1 flex-col items-center justify-center">
+          {params.success ? (
+            <div className="flex flex-col justify-center">
+              <h1 className="pb-8 text-4xl font-bold">
+                Please check your email.
+              </h1>
+              <p className="pb-6 font-bold">Almost there!</p>
+              <p className="pb-4 text-muted-foreground">
+                We've sent an email to {params.data.email}. Please check your
+                email and click the link in the email to confirm your account.
+              </p>
+              <p className="text-muted-foreground">
+                You'll be redirected to your workspace.
+              </p>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="pb-8 text-4xl font-bold">
+                Were you trying to sign in?
+              </h1>
+              <p className="text-center text-muted-foreground">
+                We weren't able to find the email you entered. Please try again.
+              </p>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-semibold leading-3">
-            Were you trying to sign in?
-          </h1>
-          <p className="text-center text-muted-foreground">
-            We weren't able to find the email you entered. Please try again.
-          </p>
+        <div className="flex flex-1 flex-col">
+          <Image src="/logo.png" alt={'logo'} width="800" height="800" />
         </div>
-      )}
+      </div>
     </div>
   );
 }
